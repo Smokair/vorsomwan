9ec613137e1fe9670979108475985237ca90d449
Smokair <95389398+Smokair@users.noreply.github.com>
2026-02-23 14:05:11 +0000

    Add files via upload

---
+++ GIT_DEVASC/PythonExperiments/netmiko/netmiko_show_version_filtered.py
@@ -0,0 +1,23 @@
+#### DEVNET SANDBOX -- ALWAYS ON
+import datetime
+print ("Current date and time: ")
+print(datetime.datetime.now())
+print("Connecting via SSH => show version")
+#
+from netmiko import ConnectHandler
+### VAR
+
+### EXEC
+sshCli = ConnectHandler(
+    device_type="cisco_ios",
+    host="192.168.56.101",
+    port="22",
+    username="cisco",
+    password="cisco123!"
+    )
+output=sshCli.send_command("show version")
+# for line in output.splitlines():
+#    if 'Cisco IOS Software' in line:
+#        ios_version = line.strip()
+
+print(type(output))
